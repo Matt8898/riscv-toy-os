a57a4a7479086bfe8c1e1c75e75c3786851f2383
Matteo Semenzato <mattew8898@gmail.com>
2021-09-28 20:45:52 +0200

    Fix script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cd build
 ninja
 cd ..
 
-../../rvtoolchain2/bin/riscv64-unknown-elf-objcopy -j .init -j .text -j .bss -j .rodata -j .data --set-section-flags .bss=alloc,load,contents -O binary build/kernel build/kernel.bin
+riscv64-elf-objcopy -j .init -j .text -j .bss -j .rodata -j .data --set-section-flags .bss=alloc,load,contents -O binary build/kernel build/kernel.bin
 mkimage -d build/kernel.bin -A riscv -O Linux -a 0x45000000 -e 0x45000000 -C none build/boot.img
 mkbootimg --kernel ./build/boot.img   --cmdline "test" --base 0x45000000 --kernel_offset 0 -o boot.img
